876f66a2e983d94d983582c1b506f2a6bac636da
RahulCh1 <chrahul101@gmail.com>
2016-02-21 18:47:36 -0600

    Added files via upload
    Got text to go through UDP sockets. Tried with image by chunking JPG image into 20 byte frames and then sending through UDP sockets, but it was lossy as heck and dropped nearly a thousand frames, therefore, corrupting the new JPG.

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/ClientA/ClientA/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.IO;
+namespace ClientA
+{
+
+    class Program
+    {
+        static int port = 1234;
+        static int toSendPort = 1235;
+        static int frameSize = 20;
+        static byte[] buffer = new byte[frameSize];
+        static int offset = 0, framenb = 0;
+        public static void getImage(FileStream fis, int totalFrames, int framenb)
+        {
+            if (framenb <= totalFrames)
+            {
+                fis.Read(buffer, offset * framenb, frameSize);
+            }
+        }
+
+        public static void runClient()
+        {
+            FileStream fis = File.Open(@"C:\Users\Rahul\Desktop\lena.jpg", FileMode.Open, FileAccess.Read);
+            long length = fis.Length;
+            int totalFrames = (int)(length / frameSize); //ignores last 20 bytes
+            Console.WriteLine(totalFrames);
+            UdpClient udpclient = new UdpClient(port);
+            byte[] addr = { 127, 0, 0, 1 };
+            IPAddress ipAddress = new IPAddress(addr);
+            IPEndPoint ep = new IPEndPoint(ipAddress, toSendPort);
+
+            //byte[] msgBytes = Encoding.ASCII.GetBytes("Hey, I am ClientA!");
+
+            for (int i = 0; i < totalFrames; i++)
+            {
+                //sends each byte to the port and address in ep
+                udpclient.Send(buffer, frameSize, ep);
+                getImage(fis, totalFrames, i);
+            }
+            fis.Close();
+            Console.WriteLine("Done!");
+            //string sent
+            /*while (true)
+            {
+                udpclient.Send(msgBytes, msgBytes.Length, ep);
+                Console.WriteLine("Sent!");
+                Thread.Sleep(1000);
+            }*/
+            Console.Read();
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starting Client A");
+            runClient();
+        }
+    }
+}
---
+++ WindowsFormsApplication1/WindowsFormsApplication1/ClientB/ClientB/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.IO;
+
+public class ClientB
+{
+    static int port = 1235;
+    static byte[] buffer;
+    static string received;
+    static FileStream fis;
+    static int framenb = 0;
+    public static void runClientReceiver()
+    {
+        UdpClient udpclient = new UdpClient(port);
+
+        IPEndPoint ep = new IPEndPoint(IPAddress.Any, port);
+
+        try
+        {
+            while (framenb <= 3000)
+            {
+                buffer = udpclient.Receive(ref ep);
+                fis.Write(buffer, 0, buffer.Length);
+                framenb++;
+                Console.WriteLine(framenb);
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.ToString());
+        }
+
+        /*while (true)
+         {
+             buffer = udpclient.Receive(ref ep);
+             received = Encoding.ASCII.GetString(buffer);
+             Console.WriteLine(received);
+         }*/
+    }
+
+    public static int Main(String[] args)
+    {
+        Console.WriteLine("Starting ClientB...");
+        fis = File.Open(@"C:\Users\Rahul\Desktop\lenaStreamed.jpg", FileMode.Append, FileAccess.Write);
+
+        runClientReceiver();
+        fis.Close();
+        return 0;
+    }
+}
